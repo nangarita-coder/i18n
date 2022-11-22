d105da312fc24849be07119011b0763aa5067e48
Nicolas A <60239309+nangarita-coder@users.noreply.github.com>
2022-11-22 09:04:44 -0500

    Taller

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import JobsList from "./components/jobsList";
 
-import JobsList from "./components/jobslist";
-
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+ReactDOM.render(
+    <IntlProvider locale="es-ES" messages= {localeEsMessages}>
+            <JobsList/>
+    </IntlProvider>, document.getElementById("root")
+);
